92d1bf145e1628a42b8960eb44e70a02978971eb
teheidoma <novakovskii2002@gmail.com>
2024-10-28 19:56:35 +0200

    aaa

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,57 @@
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    [Range(0, 10)]
     public float speed = 0.3f;
     public Transform newPosition;
+    
+    
+    
     
     void Start() {
         Debug.Log("PLAYER CREATED");
     }
 
-    // Update is called once per frame
+    
+    
+    
+    
+    
+    
     void Update() {
         if (transform.position.y < -5) {
             transform.position = newPosition.position;
         }
         Rigidbody2D r = GetComponent<Rigidbody2D>();
-        r.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, 0), ForceMode2D.Force);
-        r.AddForce(new Vector2(0, Input.GetAxis("Vertical")*2));
+
+
+        var inputForce = Input.GetAxis("Horizontal");
+        r.AddForce(new Vector2(inputForce * speed,  0), ForceMode2D.Force);
+        
+        
+        
+        
+        r.AddForce(new Vector2(0, Input.GetAxis("Vertical") * 2));
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     void OnGUI() {
         GUILayout.Label("X: " +Input.GetAxis("Horizontal") + " Y: " + Input.GetAxis("Vertical"));
